8556cadca4e592f03a7416c4c7647af5844f3b8b
GaKa00 <145324695+GaKa00@users.noreply.github.com>
2024-06-21 11:43:31 +0200

    resolved book endpoints

--- backend/resources/books/books.routes.ts
+++ backend/resources/books/books.routes.ts
@@ -2,13 +2,13 @@
 import express from "express";
 const router = express.Router();
 
-
+import { getBooks, getBookById } from "./books.controllers";
 
 // GET books: Retrieve a list of all books.
 // GET books/:id: Retrieve details of a specific book.
 
 
-router.get("/books",)
-router.get("/books/:id");
+router.get("/books", getBooks)
+router.get("/books/:id", getBookById);
 
 export default router
