2b14bc1002594a5a2019ede46bc69d08d72aed8f
Gabriel Kassarp <gkassarp@gmail.com>
2024-07-10 18:51:30 +0200

    added edit mode

--- my-project/src/components/RLModal.tsx
+++ my-project/src/components/RLModal.tsx
@@ -12,14 +12,13 @@ import {
   Image,
   Flex,
   Textarea,
+  Switch,
+  Box,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { BookDetailModalProps } from "../types/types";
 import ReactStars from "react-stars";
 import { MdSave } from "react-icons/md";
-
-
-
 
 const ReadingListModal: React.FC<BookDetailModalProps> = ({
   book,
@@ -27,9 +26,9 @@ const ReadingListModal: React.FC<BookDetailModalProps> = ({
   onClose,
 }) => {
   const [bookIdToAdd, setBookIdToAdd] = useState<number | null>(null);
-  const [editBool, setEditBool] = useState<boolean>(false)
+  const [editBool, setEditBool] = useState<boolean>(false);
 
-      const [rating, setRating] = useState<number>(0);
+  const [rating, setRating] = useState<number>(0);
 
   useEffect(() => {
     if (bookIdToAdd !== null) {
@@ -66,33 +65,41 @@ const ReadingListModal: React.FC<BookDetailModalProps> = ({
     setBookIdToAdd(book.id);
   };
 
-    const handleRatingChange = (newRating: number) => {
-      setRating(newRating);
-    };
+  const handleRatingChange = (newRating: number) => {
+    setRating(newRating);
+  };
 
-    const [summaryBool, setSummaryBool] = useState<Boolean>(false)
-     const [quoteBool, setQuoteBool] = useState<Boolean>(false);
+  const [summaryBool, setSummaryBool] = useState<boolean>(false);
+  const [quoteBool, setQuoteBool] = useState<boolean>(false);
 
+  const handleSummaryBool = () => {
+    setSummaryBool((prevSummaryBool) => !prevSummaryBool);
+  };
+  const handleQuoteBool = () => {
+    setQuoteBool((prevQuoteBool) => !prevQuoteBool);
+  };
 
-   const handleSummaryBool = () => {
-        setSummaryBool(prevSummaryBool => !prevSummaryBool);
-    }
-       const handleQuoteBool = () => {
-         setQuoteBool((prevQuoteBool) => !prevQuoteBool);
-       };
+  const handleSavedQuote = () => {
+    console.log("saved quote!");
+  };
+  const handleSavedSummary = () => {
+    console.log("saved summary!");
+  };
 
-       const handleSavedQuote = () => {
- console.log("saved quote!");
-       }
-       const handleSavedSummary = () => {
-         console.log("saved summary!")
-       }
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
       <ModalContent>
-
-        <ModalHeader textAlign="center">{book.title}</ModalHeader>
+        <ModalHeader textAlign="center">
+          <Text>{book.title}</Text>
+        </ModalHeader>
+        <Text size="sm" m={2}>Edit Mode</Text>
+        <Switch
+          size="md"
+          mx={4}
+          isChecked={editBool}
+          onChange={() => setEditBool(!editBool)}
+        />
         <Flex justify="center">
           <ReactStars
             count={5}
@@ -107,27 +114,53 @@ const ReadingListModal: React.FC<BookDetailModalProps> = ({
           <Flex align="center" justify="center" mb={4}>
             <Image src={book.image} alt={book.title} boxSize="200px" />
           </Flex>
-          {summaryBool && (
+          {editBool ? (
             <>
-              <MdSave onClick={handleSavedSummary} />{" "}
-              <Textarea placeholder="Press the save button to save your summary!" mb={4}  />{" "}
+              {summaryBool && (
+                <>
+                  <MdSave onClick={handleSavedSummary} />{" "}
+                  <Textarea
+                    placeholder="Press the save button to save your summary!"
+                    mb={4}
+                  />{" "}
+                </>
+              )}
+              {quoteBool && (
+                <>
+                  <MdSave onClick={handleSavedQuote} />
+                  <Textarea
+                    placeholder="Press the save button above to save your quote. Note. Write only one at a time!"
+                    mb={4}
+                  />
+                </>
+              )}
             </>
-          )}
-          {quoteBool && (
+          ) : (
             <>
-              <MdSave onClick={handleSavedQuote} />
-              <Textarea placeholder="Press the save button above to save your quote. 
-              Note. Write only one at a time!" mb={4}  />
+              {summaryBool && (
+                <Box mb={4} p={4} border="1px solid" borderColor="gray.300">
+                  <Text>Summary content goes here</Text>
+                </Box>
+              )}
+              {quoteBool && (
+                <Box mb={4} p={4} border="1px solid" borderColor="gray.300">
+                  <Text>Quote content goes here</Text>
+                </Box>
+              )}
             </>
           )}
         </ModalBody>
         <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={handleSummaryBool}>
-            {summaryBool ? "Hide Summary" : "Add Summary"}
-          </Button>
-          <Button colorScheme="green" mr={3} onClick={handleQuoteBool}>
-            {quoteBool ? "Hide Quote" : "Add Quote"}
-          </Button>
+          {editBool ? (
+            <>
+              <Button colorScheme="blue" mr={3} onClick={handleSummaryBool}>
+                {summaryBool ? "Hide Summary" : "Add Summary"}
+              </Button>
+              <Button colorScheme="green" mr={3} onClick={handleQuoteBool}>
+                {quoteBool ? "Hide Quote" : "Add Quote"}
+              </Button>
+            </>
+          ) : null}
           <Button colorScheme="red" mr={3} onClick={onClose}>
             Close
           </Button>
