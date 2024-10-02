be08133ed7e65bbbe67e421a9d52bcabef5de1ea
Gabriel Kassarp <gkassarp@gmail.com>
2024-10-02 15:16:35 +0200

    minor stylr change on filter boxes

--- my-project/src/components/FilterBox.tsx
+++ my-project/src/components/FilterBox.tsx
@@ -1,13 +1,17 @@
-import { Box, Checkbox, CheckboxGroup, Spacer, Stack } from "@chakra-ui/react";
+import { Box, Checkbox, CheckboxGroup, Flex, Spacer, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 
 const FilterBox = () => {
   return (
-    <Box>
+  <Flex  
+  direction={"column"}
+  gap={4}>
+
       <FactionFilter />
       <SeriesFilter />
       <OmnibusFilter />
-    </Box>
+  </Flex>
+   
   );
 };
 
@@ -16,21 +20,46 @@ const FactionFilter = () => {
 
   return (
     <Box>
+      <Text color={"white"}>Factions</Text>
       <CheckboxGroup value={value} onChange={setValue}>
         <Stack direction="row">
-          <Checkbox value="1">Adeptus Astartes</Checkbox>
-          <Checkbox value="2">Adeptus Custodes</Checkbox>
-          <Checkbox value="3">Adeptus Mechanicus</Checkbox>
-          <Checkbox value="4">Adepta Sororitas</Checkbox>
-          <Checkbox value="5">Astra Militarum</Checkbox>
+          <Checkbox value="1" color={"white"}>
+            Adeptus Astartes
+          </Checkbox>
+          <Checkbox value="2" color={"white"}>
+            Adeptus Custodes
+          </Checkbox>
+          <Checkbox value="3" color={"white"}>
+            Adeptus Mechanicus
+          </Checkbox>
+          <Checkbox value="4" color={"white"}>
+            Adepta Sororitas
+          </Checkbox>
+          <Checkbox value="5" color={"white"}>
+            Astra Militarum
+          </Checkbox>
           <Spacer />
-          <Checkbox value="6">Chaos Space Marines</Checkbox>
-          <Checkbox value="7">Aeldari</Checkbox>
-          <Checkbox value="8">Drukhari</Checkbox>
-          <Checkbox value="9">Necrons</Checkbox>
-          <Checkbox value="10">Orks</Checkbox>
-          <Checkbox value="11">T'au</Checkbox>
-          <Checkbox value="12">Tyranids</Checkbox>
+          <Checkbox value="6" color={"white"}>
+            Chaos Space Marines
+          </Checkbox>
+          <Checkbox value="7" color={"white"}>
+            Aeldari
+          </Checkbox>
+          <Checkbox value="8" color={"white"}>
+            Drukhari
+          </Checkbox>
+          <Checkbox value="9" color={"white"}>
+            Necrons
+          </Checkbox>
+          <Checkbox value="10" color={"white"}>
+            Orks
+          </Checkbox>
+          <Checkbox value="11" color={"white"}>
+            T'au
+          </Checkbox>
+          <Checkbox value="12" color={"white"}>
+            Tyranids
+          </Checkbox>
         </Stack>
       </CheckboxGroup>
     </Box>
@@ -41,13 +70,22 @@ const SeriesFilter = () => {
   const [value, setValue] = React.useState([]);
 
   return (
-    <CheckboxGroup value={value} onChange={setValue}>
-      <Stack direction="row">
-        <Checkbox value="1">First</Checkbox>
-        <Checkbox value="2">Second</Checkbox>
-        <Checkbox value="3">Third</Checkbox>
-      </Stack>
-    </CheckboxGroup>
+    <Box>
+      <Text color={"white"}>Series</Text>
+      <CheckboxGroup value={value} onChange={setValue}>
+        <Stack direction="row">
+          <Checkbox value="1" color={"white"}>
+            First
+          </Checkbox>
+          <Checkbox value="2" color={"white"}>
+            Second
+          </Checkbox>
+          <Checkbox value="3" color={"white"}>
+            Third
+          </Checkbox>
+        </Stack>
+      </CheckboxGroup>
+    </Box>
   );
 };
 
@@ -55,11 +93,17 @@ const OmnibusFilter = () => {
   const [value, setValue] = React.useState([]);
 
   return (
-    <CheckboxGroup value={value} onChange={setValue}>
-      <Stack direction="row">
-        <Checkbox value="omnibus">Omnibus</Checkbox>
-      </Stack>
-    </CheckboxGroup>
+    <Box>
+      <Text color={"white"}>Omnibus</Text>
+
+      <CheckboxGroup value={value} onChange={setValue}>
+        <Stack direction="row">
+          <Checkbox value="omnibus" color={"white"}>
+            Omnibus
+          </Checkbox>
+        </Stack>
+      </CheckboxGroup>
+    </Box>
   );
 };
 
