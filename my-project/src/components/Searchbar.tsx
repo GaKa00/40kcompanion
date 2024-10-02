8da214e50ad02336c1e1b86cc2100ba23215b045
Gabriel Kassarp <gkassarp@gmail.com>
2024-10-02 10:55:16 +0200

    added base line radio button

--- my-project/src/components/Searchbar.tsx
+++ my-project/src/components/Searchbar.tsx
@@ -8,31 +8,19 @@ import {
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 
-const SearchBar = ({ onSearch }) => {
-  const [query, setQuery] = useState("");
+const SearchBar = () => {
 
-  const handleInputChange = (e: any) => {
-    setQuery(e.target.value);
-  };
-
-  const handleSearch = () => {
-    if (onSearch) {
-      onSearch(query);
-    }
-  };
 
   return (
     <Box width="100%" maxWidth="400px" mx="auto">
       <InputGroup>
         <Input
           placeholder="Search..."
-          value={query}
-          onChange={handleInputChange}
           borderRadius="md"
           focusBorderColor="teal.400"
         />
         <InputRightElement>
-          <Button onClick={handleSearch} colorScheme="teal" size="sm">
+          <Button  colorScheme="teal" size="sm">
             <SearchIcon />
           </Button>
         </InputRightElement>
